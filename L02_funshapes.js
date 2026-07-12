6a5d6ed2be0b17ccdda3ae61e905cb8de0bfecdd
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-07-12 09:07:20 +0000

    Jul 12, 2026, 9:07 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -21,7 +21,7 @@ function draw() {
 rect(300,100,600,400)
 circle(400,200,100)
 circle(800,200,100)
-rect(600,200,100,300)
+rect(500,200,100,300)
   // Challenge 4: A rocketship using triangle() function
 
   // Challenge 5: A square face using quad() function
