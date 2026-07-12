6b2fd4ac958f909fe6a617810b01f6e190e450c5
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-07-12 08:20:49 +0000

    Jul 12, 2026, 8:20 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -7,7 +7,7 @@ function setup(){
 }
 function draw() {
   // Challenge 1: An eye using ellipse() function
-ellipse(600,300,)
+ellipse(600,300,100,100)
   // Challenge 2: A face using circle() function
 
   // Challenge 3: A house using rect() function
