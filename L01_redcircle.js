a80ff4c4778587523126d79634c0304aec69cf0e
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-07-12 07:55:10 +0000

    Jul 12, 2026, 7:55 AM

--- L01_redcircle.js
+++ L01_redcircle.js
@@ -1,11 +1,18 @@
 function setup(){
-    createCanvas(600, 400);
+    createCanvas(800, 800);
     background(220);
 }
 
 function draw(){
     fill(255, 0, 0);
-    ellipse(300,200,200,200);
-    
+    ellipse(400,200,200);
+    fill(255,255,0)
+    ellipse(400,400,200,200)
+    fill(0,255,0)
+    ellipse(400,600,200,200)
+    fill(0,0,255)
+    ellipse(200,400,200,200)
+    fill(255,0,255)
+    ellipse(600,400,200,200)
     // Todo: Challenge 3 Traffic Light
 }
