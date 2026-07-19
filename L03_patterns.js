f2724f2d752ba1b1fc741c9ae1b76f80355b21ab
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-07-19 09:19:57 +0000

    Jul 19, 2026, 9:19 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -27,7 +27,7 @@ function draw() {
 //   circle(spacing + i * spacing,200,50)
 // }
   // Task 3: Row of Circles
-circlediamiter
+let circlediamiter
 let halfwidth = 200
   // Task 4: Grid of Circles
 }
