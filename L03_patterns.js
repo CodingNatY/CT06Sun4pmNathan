7cde763919a9a5a461a660746522b4c699f06b74
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-07-19 09:26:51 +0000

    Jul 19, 2026, 9:26 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -32,6 +32,6 @@ let halfwidth = width/2
 let circlewidth = 30*5
 let halfcirclewidth = circlediamiter/2
 let gap = halfwidth - halfcirclewidth
-let radius = circled
+let radius = circlediamiter/2
   // Task 4: Grid of Circles
 }
