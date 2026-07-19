910b9a50b51fb946b89f90984779b7e737232bef
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-07-19 09:24:05 +0000

    Jul 19, 2026, 9:24 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -29,7 +29,7 @@ function draw() {
   // Task 3: Row of Circles
 let circlediamiter = 30
 let halfwidth = width/2
-let circlewidth = 30
+let circlewidth = 30*5
 let halfcirclewidth = circlediamiter/2
 
   // Task 4: Grid of Circles
