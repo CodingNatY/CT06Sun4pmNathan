9d66c63dd9dfe1fb51b95351d4765a906eb162d4
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-07-19 08:37:52 +0000

    Jul 19, 2026, 8:37 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -11,9 +11,10 @@ function draw() {
 // circle(375,200,100)
 // circle(525,200,100)
   // Task 1: Colour Gradient
-for (let i=0; i<5;i++)
+for (let i=0; i<5;i++){
 fill(i * 50)
 circle(50 + i * 50,100,40)
+}
   // Task 2: Colour Loop
 
   // Task 3: Row of Circles
