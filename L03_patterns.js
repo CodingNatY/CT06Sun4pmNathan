417d74932a908c490426ae2c07e4642c3806bce5
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-07-19 08:45:59 +0000

    Jul 19, 2026, 8:45 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -11,7 +11,7 @@ function draw() {
 // circle(375,200,100)
 // circle(525,200,100)
   // Task 1: Colour Gradient
-for (let i=0; i<10;i++){
+for (let i=0; i<9;i++){
 nostroke();
 fill(255 - i * 25);
 rect(255 + i * 35,90,40);
