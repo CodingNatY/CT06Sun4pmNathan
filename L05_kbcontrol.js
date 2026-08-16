1045a3b6d01e087533cf2fcb0212b0ca58a37724
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-08-16 08:16:39 +0000

    Aug 16, 2026, 8:16 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,17 +1,18 @@
-let size = 5;
-let colours;
-function setup(){
-    createCanvas(600,400);
-    background(220);
-}
+// let size = 5;
+// let colours;
+// function setup(){
+//     createCanvas(600,400);
+//     background(220);
+// }
 
-function mousePressed(){
-    colours = color(random(255), random(255), random(255));
-    size = 5;
-}
+// function mousePressed(){
+//     colours = color(random(255), random(255), random(255));
+//     size = 5;
+// }
 
-function mouseDragged(){
-    fill(colours)
-    circle(mouseX,mouseY,size);
-    size += 0.5;
-}
+// function mouseDragged(){
+//     fill(colours)
+//     circle(mouseX,mouseY,size);
+//     size += 0.5;
+// }
+
