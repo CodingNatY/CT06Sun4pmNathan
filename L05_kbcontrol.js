1240f6b22b8aa240f7a92c09f6aad8d038088def
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-08-16 08:26:33 +0000

    Aug 16, 2026, 8:26 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -16,15 +16,20 @@ function setup(){
 //     size += 0.5;
 // }
 
-function keyPressed(){
-    let numCircles = floor(random(5,21));
-    for(let i=0; i < numCircles; i++);
-        let x = random(width);
-        let y = random(height);
-        let size = random(5,40);
-        fill(random(255),random(255),random(255))
-        circle(x,y,size);
-}
-function keyRelesased(){
-    background(220);
+// function keyPressed(){
+//     let numCircles = floor(random(5,21));
+//     for(let i=0; i < numCircles; i++);
+//         let x = random(width);
+//         let y = random(height);
+//         let size = random(5,40);
+//         fill(random(255),random(255),random(255))
+//         circle(x,y,size);
+// }
+// function keyRelesased(){
+//     background(220);
+// }
+
+function draw(){
+    if(key === 'c' || key === 'C')
+        
 }
