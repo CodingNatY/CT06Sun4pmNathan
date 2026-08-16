55113c5f6313cfbabb7f6fd79cda080eb84bd4e1
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-08-16 08:23:20 +0000

    Aug 16, 2026, 8:23 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -23,5 +23,5 @@ function keyPressed(){
         let y = random(height);
         let size = random(5,40);
         fill(random(255),random(255),random(255))
-        circle(x)
+        circle(x,y,size);
 }
