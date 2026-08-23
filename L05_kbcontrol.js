190e32e26b0f18feab40845a9697f0ef300b80c5
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-08-23 09:27:25 +0000

    Aug 23, 2026, 9:27 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -40,7 +40,9 @@ function keyReleased(){
 
 function draw(){
     fill(shapeColor);
-    if(key === 'r' || key === 'R'){}
+    if(key === 'r' || key === 'R'){
+        
+    }
 
 //     if(key === 'c' || key === 'C'){
 //          circle(100,100,80);
