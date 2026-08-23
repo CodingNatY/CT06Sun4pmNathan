576f14f0f0c528ace7b350de189e1031d7d84c0d
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-08-23 08:31:10 +0000

    Aug 23, 2026, 8:31 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -10,19 +10,18 @@ function setup(){
     createCanvas(400,400);
     background(220);
 }
-function draw(){
-    if(keyCode === 38){
-        y -= 5;
-    }
-    if(keyCode === 37){
-        x-=5;
-    }
-    if(keyCode === 39){
-        x+=5;
-    }
-    if(keyCode === 40){
-        y+=5;
-    }
-    circle(x,y,50);
-
+// function draw(){
+//     if(keyCode === 38){
+//         y -= 5;
+//     }
+//     if(keyCode === 37){
+//         x-=5;
+//     }
+//     if(keyCode === 39){
+//         x+=5;
+//     }
+//     if(keyCode === 40){
+//         y+=5;
+//     }
+//     circle(x,y,50);
 }
