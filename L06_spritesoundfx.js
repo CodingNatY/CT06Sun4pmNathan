f055ce5b78c0b1e99f6acd26e56290552df03004
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-08-23 09:05:11 +0000

    Aug 23, 2026, 9:05 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -16,7 +16,7 @@ function setup(){
 }
 function draw(){
      background(220);
-     if(staticImageX<=0 || staticImageX >= 400 || staticImageY<=0 || staticImageY)
+     if(staticImageX<=0 || staticImageX >= 400 || staticImageY<=0 || staticImageY>=400)
     if(keyIsDown(LEFT_ARROW)){
         staticImageX-=staticSpeed;
     }
