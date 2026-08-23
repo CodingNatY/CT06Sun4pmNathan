4f7bfd66fb5f6f387946f9044aa047bdb3105ede
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-08-23 08:25:03 +0000

    Aug 23, 2026, 8:25 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -4,7 +4,7 @@ function setup(){
     createCanvas(400,400);
     background(220);
 }
-let soundEffect, bgMusic, 
+let soundEffect, bgMusic, static
 function draw(){
     if(keyCode === 38){
         y -= 5;
