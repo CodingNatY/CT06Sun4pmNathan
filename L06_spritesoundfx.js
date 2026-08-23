8d720ac2d91ce2cfd2fb9acfe6ac90598adbda3b
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-08-23 08:57:11 +0000

    Aug 23, 2026, 8:57 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -28,7 +28,9 @@ function draw(){
         if(keyIsDown(DOWN_ARROW)){
         staticImageY+=staticSpeed
     }
-    constrain(staticImageX)
+    constrain(staticImageX,50,350)
+    constrain(staticImageY,50,350)
+    )
     image(staticImage,staticImageX,staticImageY,50,50);
 }
 function keyPressed(){
