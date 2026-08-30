262b52858a4b7c64ab7baa4cc4eab3a4b7148f0c
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-08-30 08:10:38 +0000

    Aug 30, 2026, 8:10 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,8 +1,9 @@
 function setup(){
     createCanvas(400,400);}
 
-function draw(){
-     background(220);
     let soundEffect = loadSound('assets/pop.mp3');
     let staticImage  = loadImage('assets/pico-a.png');
+function draw(){
+     background(220);
+
 }
