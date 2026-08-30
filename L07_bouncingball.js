6a5ac62fda3c2b90de7e271c319444f0d687bf38
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-08-30 08:38:17 +0000

    Aug 30, 2026, 8:38 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -34,7 +34,7 @@ function draw(){
 //     if(key === " "){
 //         picowidth/=2;
 //         popSound.loop();
-createCanvas()
+b
     circle(ballx,bally,ballsize);
     ballx += ballspeedx;
     bally += ballspeedy;
