88e865003edb37aa6eb35efcfa29dc30c6916df9
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-08-30 08:27:53 +0000

    Aug 30, 2026, 8:27 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,35 +1,35 @@
-let pico;
-let popSound;
-let x = 200;
-let y = 200;
-let picowidth = 100;
-function setup(){
-    createCanvas(400,400);}
-function preload(){
-    let soundEffect = loadSound('assets/pop.mp3');
-    let staticImage  = loadImage('assets/pico-a.png');  
-}
+// let pico;
+// let popSound;
+// let x = 200;
+// let y = 200;
+// let picowidth = 100;
+// function setup(){
+//     createCanvas(400,400);}
+// function preload(){
+//     let soundEffect = loadSound('assets/pop.mp3');
+//     let staticImage  = loadImage('assets/pico-a.png');  
+// }
 
-function draw(){
-     background(220);
-    if(keyIsDown(LEFT_ARROW)){
-        x -= 3;
-    }
-    if(keyIsDown(RIGHT_ARROW)){
-        x += 3;
-    }
-    if(keyIsDown(UP_ARROW)){
-        y-=3;
-    }
-    if(keyIsDown(DOWN_ARROW)){
-        y+=3;
-    }
-    Image(pico,x,y,picowidth,100);
-}
-function keyPressed(){
-    if(key === " "){
-        picowidth/=2;
-        popSound.loop();
-    }
+// function draw(){
+//      background(220);
+//     if(keyIsDown(LEFT_ARROW)){
+//         x -= 3;
+//     }
+//     if(keyIsDown(RIGHT_ARROW)){
+//         x += 3;
+//     }
+//     if(keyIsDown(UP_ARROW)){
+//         y-=3;
+//     }
+//     if(keyIsDown(DOWN_ARROW)){
+//         y+=3;
+//     }
+//     Image(pico,x,y,picowidth,100);
+// }
+// function keyPressed(){
+//     if(key === " "){
+//         picowidth/=2;
+//         popSound.loop();
+//     }
 
-}
+// }
