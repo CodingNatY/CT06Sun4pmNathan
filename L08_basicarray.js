7e8bba26ea788b2a3fe6ff26b70feab275a302b1
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-09-13 09:04:44 +0000

    Sep 13, 2026, 9:04 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -2,7 +2,7 @@
 // let y=200;
 // let speed=3;
 let ypos = 500;
-let food=["AAAAAAA","aaaaaaa","AaAaAa","",""];
+let food=["AAAAAAA","aaaaaaa","AaAaAa","","","ygutibyui",];
 function setup(){
     createCanvas(500,500);
  
