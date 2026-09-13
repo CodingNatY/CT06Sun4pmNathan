be07577f7227779bce5801ee357f90f240bf4916
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-09-13 09:19:51 +0000

    Sep 13, 2026, 9:19 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -6,7 +6,7 @@ let food=["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a
 let linegap = 30;
 let bgSound;
 function preload(){
-    bgSound = loadSound
+    bgSound = loadSound('ass')
 }
 function setup(){
     createCanvas(500,500);
