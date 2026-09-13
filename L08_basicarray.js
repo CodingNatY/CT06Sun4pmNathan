59fe1d5f0282f7b45a57c3984d11aaeb91af7b6c
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-09-13 08:47:50 +0000

    Sep 13, 2026, 8:47 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -10,14 +10,14 @@ function draw(){
     
   
 
-//   square(x,y,40);
-//   x+=speed;
-//     if(x<0 || x>=width-40){
-//         speed = -speed;
-//         fill(random(255),random(255),random(255));
-//     }
-textSize(32);
-textAlign(RIGHT)
+// //   square(x,y,40);
+// //   x+=speed;
+// //     if(x<0 || x>=width-40){
+// //         speed = -speed;
+// //         fill(random(255),random(255),random(255));
+// //     }
+// textSize(32);
+// textAlign(RIGHT)
 // text("nathan",50,50);
 // text("9",50,100);
 // text("minecraft",50,150);
