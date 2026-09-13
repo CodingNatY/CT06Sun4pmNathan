03e351bab016c227c89e6cc2186f7b055534ac18
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-09-13 08:26:34 +0000

    Sep 13, 2026, 8:26 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,21 +1,21 @@
-// let x=200;
-// let y=200;
-// let speed=3;
-// function setup(){
-//     createCanvas(400,400);
+let x=200;
+let y=200;
+let speed=3;
+function setup(){
+    createCanvas(400,400);
   
-// }
-// function draw(){
+}
+function draw(){
     
   
-//     background(220);
-//   square(x,y,40);
-//   x+=speed;
-//     if(x<0 || x>=width-40){
-//         speed = -speed;
-//         fill(random(255),random(255),random(255));
-//     }
+    background(220);
+  square(x,y,40);
+  x+=speed;
+    if(x<0 || x>=width-40){
+        speed = -speed;
+        fill(random(255),random(255),random(255));
+    }
 
 
 
-// }
+}
