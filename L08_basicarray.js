d83219b81e897c30065fe71a76d301e4d71b3c7d
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-09-13 09:14:45 +0000

    Sep 13, 2026, 9:14 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -6,7 +6,9 @@ let food=[];
 let linegap = 30
 function setup(){
     createCanvas(500,500);
-    for(let i=0;i<26;i++){}
+    for(let i=0;i<26;i++){
+        food[i]="a"+i;
+    }
 }
 function draw(){
     
