55dc325e46dff6204f70ad5e7a63d1a5c1e8e69a
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-09-13 08:35:41 +0000

    Sep 13, 2026, 8:35 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -16,9 +16,9 @@ function draw(){
 //         fill(random(255),random(255),random(255));
 //     }
 textSize(32);
-textAlign()
-// text("nathan",50,50);
-// text("9",50,100);
-// text("minecraft",50,150);
+textAlign(RIGHT)
+text("nathan",50,50);
+text("9",50,100);
+text("minecraft",50,150);
 
 }
