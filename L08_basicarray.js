640cd344e9921da1d1ed3434a8b9ff8f695e81e6
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-09-13 08:57:23 +0000

    Sep 13, 2026, 8:57 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,7 +1,7 @@
-let x=200;
-let y=200;
-let speed=3;
-let food=["x","y","speed"];
+// let x=200;
+// let y=200;
+// let speed=3;
+let food=["AAAAAAA","aaaaaaa","AaAaAa"];
 function setup(){
     createCanvas(2000,2000);
     background(220)
@@ -10,20 +10,21 @@ function draw(){
     
   
 
-  square(x,y,40);
+//   square(x,y,40);
 
-  x+=speed;
-    if(x<0 || x>=width-40){
-        speed = -speed;
-        fill(random(255),random(255),random(255));
-     }
+//   x+=speed;
+//     if(x<0 || x>=width-40){
+//         speed = -speed;
+//         fill(random(255),random(255),random(255));
+ //    }
 textSize(32);
 fill(0)
 textAlign(LEFT);
 // text("nathan",50,50);
 // text("9",50,100);
 // text("minecraft",50,150);
-for(let i=0 ; i<food.length ;i++ ){
-    text(food[i],50,100+i*50);
-}
+// for(let i=0 ; i<food.length ;i++ ){
+//     text(food[i],50,100+i*50);
+// }
+
 }
