2cb7c1ea3274b32f89e5dcc091f3d1857083cb93
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-09-13 09:20:28 +0000

    Sep 13, 2026, 9:20 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -6,7 +6,7 @@ let food=["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a
 let linegap = 30;
 let bgSound;
 function preload(){
-    bgSound = loadSound('assets/star_wars')
+    bgSound = loadSound('assets/star_wars_theme_')
 }
 function setup(){
     createCanvas(500,500);
