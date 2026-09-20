bcbc18ca64b0e34483b09527dc138e709ccabe34
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-09-20 08:57:33 +0000

    Sep 20, 2026, 8:57 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -38,7 +38,7 @@ function updateCountdown(){
     }
     if(countdown==0){
         interval=setInterval(updateCountdown,500);
-        background(random(255),random(255),random())
+        background(random(255),random(255),random(255));
         bgmusic.play()
     }
 }
