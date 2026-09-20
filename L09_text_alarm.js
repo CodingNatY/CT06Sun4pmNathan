1e57fe807fb66c73d442358a42dfcb162c4663cd
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-09-20 09:13:33 +0000

    Sep 20, 2026, 9:13 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -39,7 +39,7 @@ function updateCountdown(){
         countdown-=1;
     }else{
         clearInterval(interval);
-        setInterval(alarm,1);
+        setInterval(alarm,500);
     }
 }
 function alarm(){
