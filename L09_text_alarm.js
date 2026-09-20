b289bdbae45eacde970a893e72916df296818dfe
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-09-20 09:25:45 +0000

    Sep 20, 2026, 9:25 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -32,7 +32,9 @@ function draw(){
     textSize(32);
     text("click to start the timer",100,300);
     text("press space to restart timer",100,350);
-    if(key===' ')
+    if(key===' '){
+        
+    }
 }
 
 function mousePressed(){
