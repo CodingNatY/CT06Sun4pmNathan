5a8be776f43702654e75402c05ebd76e094af64e
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-09-20 08:25:00 +0000

    Sep 20, 2026, 8:25 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -6,10 +6,11 @@ function setup(){
 
 }
 function draw(){
+    background(220);
     fill(0);
     textSize(32);
     text("bounce",x,y);
-    background(220);
+
     if(x==0 || x==400){
         speed = -speed;
     }
