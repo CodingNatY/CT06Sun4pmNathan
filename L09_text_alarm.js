238e03bebfe9d6543d425353ae6a401c40088375
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-09-20 08:55:10 +0000

    Sep 20, 2026, 8:55 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -4,7 +4,7 @@
 let countdown = 5;
 let interval;
 function preload(){
-    let bgmusic=bossaNova.mp3
+    let bgmusic=bossaNova.mp3;
 }
 function setup(){
     createCanvas(600,400);
@@ -38,6 +38,6 @@ function updateCountdown(){
     }
     if(countdown==0){
         interval=setInterval(updateCountdown,500);
-
+        
     }
 }
