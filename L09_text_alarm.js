fea938f8017663616fdcd78c1b451695499c9314
CodingNatY <129062676+CodingNatY@users.noreply.github.com>
2026-09-20 08:29:29 +0000

    Sep 20, 2026, 8:29 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,13 +1,14 @@
 // let speed = 3;
 // let x=50;
 // let y=200;
+
 function setup(){
     createCanvas(600,400);
 }
 function draw(){
     background(220);
     // fill(0);
-    // textSize(32);
+    textSize(32);
     // text("bounce",x,y);
     // x+=speed;
     // if(x<0 || x>600-60){
